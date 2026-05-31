244b4c47c6d686955bdd4b2c798f0d2cc842133d
Jiale Fan <jiale@leggedrobotics.com>
2026-05-31 15:07:13 +0200

    implemented contact force sensing and visualization on knees (outer shell)

--- knee_eflesh/bin/knee_contact_force_visualizer.py
+++ knee_eflesh/bin/knee_contact_force_visualizer.py
@@ -1,91 +1,117 @@
 #!/usr/bin/env python3
 
+import math
+
 import rospy
-from gazebo_msgs.msg import ContactsState
-from geometry_msgs.msg import Point
+import tf
+from geometry_msgs.msg import Point, PointStamped
 from visualization_msgs.msg import Marker, MarkerArray
+
+from dynasense.msg import KneeVisMsg
 
 
 class KneeContactForceVisualizer:
-    """Visualize projected knee-contact force as arrow markers."""
+    """Visualize knee contact as arrow (force) or blue sphere (no contact)."""
 
     LEGS = ("LF", "RF", "LH", "RH")
-    LEN_COEFF = 0.01  # [m/N] hardcoded scale from projected force to arrow length.
-    ARROW_RADIUS = 0.008
-    ARROW_HEAD_RADIUS = 0.015
-    MIN_ARROW_LEN = 1e-4
-    ARROW_DIR = (0.0, 1.0, 0.0)  # +Y axis in each <leg>_shank_fixed frame.
+
+    LEN_COEFF = 0.001  # [m/N]
+    MIN_ARROW_LEN = 0.05
+    MAX_ARROW_LEN = 0.3
+    ARROW_SHAFT_DIAM = 0.05
+    ARROW_HEAD_DIAM = 0.1
+    ARROW_HEAD_LEN = 0.02
+    SPHERE_DIAM = 0.03
 
     def __init__(self):
-        self.marker_topic = rospy.get_param("~marker_topic", "/knee_eflesh/knee_contact_force_markers")
-        self.contact_topic_format = rospy.get_param("~contact_topic_format", "/contacts/{leg}/knee_cylinder")
-        self.frame_fallback = rospy.get_param("~frame_fallback", "base")
+        self.input_topic = rospy.get_param("~input_topic", "/knee_eflesh/knee_vis")
+        self.output_topic = rospy.get_param("~marker_topic", "/knee_eflesh/knee_contact_force_markers")
+        self.target_frame = rospy.get_param("~target_frame", "odom")
         self.marker_lifetime = rospy.Duration(rospy.get_param("~marker_lifetime_sec", 0.2))
 
-        self.pub = rospy.Publisher(self.marker_topic, MarkerArray, queue_size=10)
+        self.tf_listener = tf.TransformListener()
+        self.pub = rospy.Publisher(self.output_topic, MarkerArray, queue_size=20)
+        self.sub = rospy.Subscriber(self.input_topic, KneeVisMsg, self._msg_cb, queue_size=50)
+        rospy.loginfo("knee_contact_force_visualizer listening to %s", self.input_topic)
 
-        for leg in self.LEGS:
-            topic = self.contact_topic_format.format(leg=leg)
-            rospy.Subscriber(topic, ContactsState, self._contact_cb, callback_args=leg, queue_size=1)
-            rospy.loginfo("knee_contact_force_visualizer subscribed to %s", topic)
+    def _msg_cb(self, msg):
+        if msg.leg_id not in self.LEGS:
+            rospy.logwarn_throttle(2.0, "knee_contact_force_visualizer: unknown leg_id '%s'", msg.leg_id)
+            return
 
-    def _contact_cb(self, msg, leg):
-        marker = self._build_marker(msg, leg)
-        self.pub.publish(MarkerArray(markers=[marker]))
+        marker = self._build_marker(msg)
+        if marker is not None:
+            self.pub.publish(MarkerArray(markers=[marker]))
 
-    def _build_marker(self, msg, leg):
+    def _build_marker(self, msg):
+        leg_frame = "{}_shank_fixed".format(msg.leg_id)
         marker = Marker()
         marker.header.stamp = rospy.Time.now()
-        marker.header.frame_id = msg.header.frame_id if msg.header.frame_id else self.frame_fallback
-        marker.ns = "knee_contact_force"
-        marker.id = self.LEGS.index(leg)
-        marker.type = Marker.ARROW
+        marker.header.frame_id = self.target_frame
+        marker.ns = "knee_contact_vis"
+        marker.id = self.LEGS.index(msg.leg_id)
         marker.lifetime = self.marker_lifetime
         marker.frame_locked = False
         marker.pose.orientation.w = 1.0
-        marker.scale.x = self.ARROW_RADIUS
-        marker.scale.y = self.ARROW_HEAD_RADIUS
-        marker.scale.z = self.ARROW_HEAD_RADIUS
+
+        origin_leg = Point(x=0.0, y=0.0, z=0.0)
+        origin_target = self._transform_point(origin_leg, leg_frame, self.target_frame)
+        if origin_target is None:
+            return None
+
+        if msg.magnitude <= 0.0:
+            marker.type = Marker.SPHERE
+            marker.action = Marker.ADD
+            marker.pose.position = origin_target
+            marker.scale.x = self.SPHERE_DIAM
+            marker.scale.y = self.SPHERE_DIAM
+            marker.scale.z = self.SPHERE_DIAM
+            marker.color.r = 0.1
+            marker.color.g = 0.3
+            marker.color.b = 1.0
+            marker.color.a = 0.95
+            return marker
+
+        arrow_len = max(self.MIN_ARROW_LEN, min(self.MAX_ARROW_LEN, self.LEN_COEFF * msg.magnitude))
+        dir_x = math.sin(msg.angle)
+        dir_z = math.cos(msg.angle)
+        end_leg = Point(x=arrow_len * dir_x, y=0.0, z=arrow_len * dir_z)
+        end_target = self._transform_point(end_leg, leg_frame, self.target_frame)
+        if end_target is None:
+            return None
+
+        marker.type = Marker.ARROW
+        marker.action = Marker.ADD
+        marker.points = [origin_target, end_target]
+        marker.scale.x = self.ARROW_SHAFT_DIAM
+        marker.scale.y = self.ARROW_HEAD_DIAM
+        marker.scale.z = self.ARROW_HEAD_LEN
         marker.color.r = 0.95
         marker.color.g = 0.2
         marker.color.b = 0.2
         marker.color.a = 0.95
-
-        best_state = None
-        best_proj_mag = -1.0
-        for state in msg.states:
-            if not state.contact_positions:
-                continue
-            projected_force = state.total_wrench.force.x * self.ARROW_DIR[0] + state.total_wrench.force.y * self.ARROW_DIR[1] + state.total_wrench.force.z * self.ARROW_DIR[2]
-            projected_force_mag = abs(projected_force)
-            if projected_force_mag > best_proj_mag:
-                best_proj_mag = projected_force_mag
-                best_state = state
-
-        if best_state is None:
-            marker.action = Marker.DELETE
-            return marker
-
-        contact_pos = best_state.contact_positions[0]
-        projected_force = (
-            best_state.total_wrench.force.x * self.ARROW_DIR[0]
-            + best_state.total_wrench.force.y * self.ARROW_DIR[1]
-            + best_state.total_wrench.force.z * self.ARROW_DIR[2]
-        )
-
-        direction_sign = 1.0 if projected_force >= 0.0 else -1.0
-        arrow_len = max(self.MIN_ARROW_LEN, self.LEN_COEFF * abs(projected_force))
-
-        start = Point(x=contact_pos.x, y=contact_pos.y, z=contact_pos.z)
-        end = Point(
-            x=start.x + direction_sign * arrow_len * self.ARROW_DIR[0],
-            y=start.y + direction_sign * arrow_len * self.ARROW_DIR[1],
-            z=start.z + direction_sign * arrow_len * self.ARROW_DIR[2],
-        )
-
-        marker.action = Marker.ADD
-        marker.points = [start, end]
         return marker
+
+    def _transform_point(self, point, source_frame, target_frame):
+        if source_frame == target_frame:
+            return point
+
+        stamped = PointStamped()
+        stamped.header.stamp = rospy.Time(0)
+        stamped.header.frame_id = source_frame
+        stamped.point = point
+
+        try:
+            transformed = self.tf_listener.transformPoint(target_frame, stamped)
+            return transformed.point
+        except (tf.Exception, tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            rospy.logwarn_throttle(
+                2.0,
+                "knee_contact_force_visualizer: cannot transform point from %s to %s",
+                source_frame,
+                target_frame,
+            )
+            return None
 
 
 def main():
